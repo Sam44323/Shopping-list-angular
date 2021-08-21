91699ffd23a4f5842c42cd5b02e4a7f674f908db
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-21 17:38:34 +0530

    add: added recipe detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipesListComponent
+    RecipesListComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule,
