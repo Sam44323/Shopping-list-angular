a11030488ada82fefa299222572dd5680110857d
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-21 21:56:15 +0530

    add: added a demo recipe for recipes list

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.scss']
+  styleUrls: ['./recipes.component.scss'],
 })
 export class RecipesComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'This is a recipe',
+      'This was made by an idiot like you',
+      'https://picsum.photos/200/300'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
