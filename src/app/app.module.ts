36f7e9b612da7c90feb1d7f76463d7134a2e08fa
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-21 17:35:13 +0530

    add: added recipes-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipesListComponent
   ],
   imports: [
     BrowserModule,
