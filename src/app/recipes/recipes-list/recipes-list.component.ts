279a8601dcb4a29cc2c798489c4185f7125b9212
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-21 21:58:05 +0530

    add: added a recipe list in recipes-list component

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.scss']
+  styleUrls: ['./recipes-list.component.scss'],
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'This is a recipe',
+      'This was made by an idiot like you',
+      'https://picsum.photos/200/300'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -7,14 +6,6 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.component.scss'],
 })
 export class RecipesComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'This is a recipe',
-      'This was made by an idiot like you',
-      'https://picsum.photos/200/300'
-    ),
-  ];
-
   constructor() {}
 
   ngOnInit(): void {}
