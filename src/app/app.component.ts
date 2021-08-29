498e86699349419a4a5a40f503cf9895ebf71e3d
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-30 01:02:36 +0530

    add: added data binder and sender for updating the nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Shopping-list-angular';
+  currentNav: string = 'Recipes';
+
+  setCurrentNav = (data: string) => {
+    console.log(data);
+    this.currentNav = data;
+  };
 }
--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Output('navHandler') navElementHandler: EventEmitter<string> =
+    new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  changeNav = (data: string) => {
+    this.navElementHandler.emit(data);
+  };
 }
