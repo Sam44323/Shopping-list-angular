6ed39df13c845a426d3228c27e0f8905ff5b5bdb
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-30 01:34:47 +0530

    update: updated the content for recipe-item component and added data binding

--- src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.scss']
+  styleUrls: ['./recipe-item.component.scss'],
 })
 export class RecipeItemComponent implements OnInit {
+  @Input('recipe') recipeItem: {
+    name: string;
+    imagePath: string;
+    description: string;
+  };
+  @Input('index') index: number;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
