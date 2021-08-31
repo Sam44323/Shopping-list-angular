e4bfbc4e1507761a3ab62fdf6ae967177ffd9732
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-31 23:05:24 +0530

    update: updated the directive for dropdown

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,12 +1,19 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false; // will add the class only when isOpen is true
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
-  constructor() {}
+  constructor(private elRef: ElementRef) {}
 }
