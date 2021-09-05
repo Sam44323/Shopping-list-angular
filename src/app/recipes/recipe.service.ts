a38f2cd749d110bc21a268f1709ef5b4d070f054
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-05 21:59:45 +0530

    add: added recipes data and getter for the recipes service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable()
-export class RecipeService {}
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'This is a recipe',
+      'This was made by an idiot like you',
+      'https://picsum.photos/200/300'
+    ),
+    new Recipe(
+      'This is another recipe',
+      'This was made by another idiot like you',
+      'https://picsum.photos/200/300'
+    ),
+    new Recipe(
+      'This is also a recipe',
+      'This was also made by an idiot like you',
+      'https://picsum.photos/200/300'
+    ),
+  ];
+
+  getRecipes = () => this.recipes.slice(); // returning the copy of the recipes array instead of the exact array pointer
+}
